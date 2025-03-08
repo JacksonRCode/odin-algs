16c2867554be40d1a82f3f3ca7029218f267413b
JacksonRCode <jackdareid@gmail.com>
2025-03-08 10:58:41 -0500

    Finish recursive fib sequence

--- fib.js
+++ fib.js
@@ -1,4 +1,6 @@
-const fibIter = (n) => {
+// Iterative version
+
+const fibsIter = (n) => {
   let arr = [0];
 
   for (let i = 1; i < n; i++) {
@@ -12,4 +14,15 @@ const fibIter = (n) => {
   return arr;
 };
 
-console.log(fibIter(8));
+// Recursive version
+
+const fibsRec = (n, prev2 = null, prev = null) => {
+  if (n === 0) return [];
+  if (prev === null) return [0].concat(fibsRec(n - 1, null, 0));
+  if (prev2 === null) return [1].concat(fibsRec(n - 1, 0, 1));
+
+  let newVal = prev2 + prev;
+  return [newVal].concat(fibsRec(n - 1, prev, newVal));
+};
+
+console.log(fibsRec(32));

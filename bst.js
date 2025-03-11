7f9fa7f0cfbf06ebee95b89666e93ec4a331a963
JacksonRCode <jackdareid@gmail.com>
2025-03-11 13:31:37 -0400

    Implement insert and delete functions

--- bst.js
+++ bst.js
@@ -19,6 +19,14 @@ const Node = (value, left = null, right = null) => {
   const getLeft = () => _left;
   const getRight = () => _right;
 
+  const numKids = () => {
+    if (left || right) {
+      if (left && right) return 2;
+      return 1;
+    }
+    return 0;
+  };
+
   return {
     getValue,
     setValue,
@@ -26,12 +34,13 @@ const Node = (value, left = null, right = null) => {
     setRight,
     getLeft,
     getRight,
+    numKids,
   };
 };
 
 const Tree = (arr) => {
   let _arr;
-  let _root;
+  let _root = null;
 
   const buildTree = (array, start = 0, end = array.length - 1) => {
     if (start > end) return null;
@@ -65,12 +74,112 @@ const Tree = (arr) => {
     }
   };
 
-  const insert = (newItem) => {
+  const insert = (value) => {
     // blah blah insert
+    const newNode = Node(value);
+
+    if (!_root) {
+      _root = newNode;
+      return;
+    }
+
+    // let value = newItem.getValue();
+    let prev;
+    let curr = _root;
+    while (curr !== null) {
+      prev = curr;
+      let left = curr.getLeft();
+      let right = curr.getRight();
+      let currValue = curr.getValue();
+
+      if (!left || !right) {
+        // If one of them is null, set curr to the other one.
+        curr = !right ? left : right;
+      } else {
+        curr = value < currValue ? left : right;
+      }
+    }
+
+    // If previous node has left child, set right
+    // This makes default setting to left.
+    const prevValue = prev.getValue();
+    if (prevValue < value) {
+      prev.setRight(newNode);
+      return;
+    }
+    prev.setLeft(newNode);
+    return;
   };
 
-  const deleteItem = (value) => {
+  const deleteItem = (value, curr = _root) => {
     // blah blah delete
+
+    /*
+      1. Find node
+    */
+    let prev;
+    let direction;
+    // Find the node to be deleted and set it to curr
+    while (curr.getValue() !== value) {
+      prev = curr;
+      let currValue = curr.getValue();
+      if (value > currValue) {
+        curr = curr.getRight();
+        direction = "right";
+      } else {
+        curr = curr.getLeft();
+        direction = "left";
+      }
+
+      if (curr === null) return "That item is not in hash map";
+    }
+
+    // Case 1: no children;
+    if (curr.numKids() === 0) {
+      if (direction === "right") {
+        prev.setRight(null);
+      } else {
+        prev.setLeft(null);
+      }
+
+      return "Removed " + value;
+    }
+
+    // Case 2: single child
+    else if (curr.numKids() === 1) {
+      let replace = !curr.getLeft() ? curr.getRight() : curr.getLeft();
+      if (direction === "right") {
+        prev.setRight(replace);
+      } else {
+        prev.setLeft(replace);
+      }
+
+      return "Removed " + value;
+    }
+
+    // Case 3: two children\
+    else {
+      // Find smallest element in right side of tree from curr
+      let replace = curr.getRight();
+      let replacePrev;
+      while (replace.getLeft() !== null) {
+        replacePrev = replace;
+        replace = replace.getLeft();
+      }
+      // Set previous elements left child to smallest elements right child
+      replacePrev.setLeft(replace.getRight());
+
+      // Set prvious of deletion item to
+      replace.setLeft(curr.getLeft());
+      replace.setRight(curr.getRight());
+      if (direction === "right") {
+        prev.setRight(replace);
+      } else {
+        prev.setLeft(replace);
+      }
+
+      return "Removed " + value;
+    }
   };
 
   const find = (value) => {
@@ -117,9 +226,15 @@ const Tree = (arr) => {
 
   init();
 
-  return { prettyPrint };
+  return {
+    prettyPrint,
+    insert,
+    deleteItem,
+  };
 };
 
 let arr = [1, 7, 4, 23, 8, 9, 3, 5, 67, 6345, 324];
 let spruce = Tree(arr);
-// spruce.prettyPrint();
+spruce.insert(13);
+console.log(spruce.deleteItem(0));
+spruce.prettyPrint();
